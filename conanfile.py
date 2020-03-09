bc7ae526e1de32088a9fd08583e833282d388623
Frank Lange <frank.lange@holoplot.com>
2020-03-09 09:26:08 +0100

    Add conan recipe

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, tools, CMake
+import os
+
+def get_version():
+    major_version = '0'
+    if os.environ.get("PACKAGE_VERSION", None):
+        return f"{major_version}.{os.environ['PACKAGE_VERSION']}"
+    return major_version
+
+
+class ShieldConan(ConanFile):
+    name = "sw__shield"
+    version = get_version()
+    topics = ("shield", "utility")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    requires = []
+    generators = "cmake"
+    exports_sources = "*"
+        
+    def package(self):
+        self.copy("*", src="include", dst="include")
+
+    def package_info(self):
+        self.info.header_only()
