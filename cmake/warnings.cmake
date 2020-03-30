b7c15e5b06d2993399cde068d4d14de37a4bd581
stephan zunhammer <stephan.zunhammer@holoplot.com>
2020-03-30 12:03:39 +0200

    added warnings cmake file to shield

---
+++ cmake/warnings.cmake
@@ -0,0 +1,17 @@
+function(enable_warnings TARGET SCOPE)
+    if (APPLE OR UNIX)
+        target_compile_options(${TARGET} ${SCOPE} -Wall -Wextra -pedantic)
+    elseif(MSVC)
+        target_compile_options(${TARGET} ${SCOPE} /W4)
+    endif()
+endfunction()
+
+function(warnings_as_errors TARGET SCOPE)
+    enable_warnings(${TARGET} ${SCOPE})
+
+    if (APPLE OR UNIX)
+        target_compile_options(${TARGET} ${SCOPE} -Werror)
+    elseif(MSVC)
+        target_compile_options(${TARGET} ${SCOPE} /WX)
+    endif()
+endfunction()
